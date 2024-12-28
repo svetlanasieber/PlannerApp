4fd2dab9517a81601f9dc8720eea4d577ad600af
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-28 13:32:15 +0100

    Update HomeController.java

--- src/main/java/com/plannerapp/controller/HomeController.java
+++ src/main/java/com/plannerapp/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 import java.util.Set;
 
 @Controller
